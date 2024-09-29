724380d22cf6a8ff09553ca8a843e1899033b976
khalidaliyeva <148062237+khalidaliyeva@users.noreply.github.com>
2024-09-29 21:02:32 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,47 @@
 plugins {
-    id("java")
+    java
+    id("jacoco")
+    id("org.sonarqube") version "3.3" 
 }
-
-group = "org.example"
+ 
+group = "bhos-qa" 
 version = "1.0-SNAPSHOT"
-
+ 
 repositories {
-    mavenCentral()
+    mavenCentral() 
 }
-
+ 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.2") 
 }
+ 
 
+jacoco {
+    toolVersion = "0.8.9" 
+}
+ 
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true) 
+        html.required.set(true) 
+    }
+}
+ 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+ 
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "bhos-qa_lab-3-EsmiraFarziyeva2004")
+        property("sonar.organization", "bhos-qa")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", "29a300f5cc16200a0d11b85c41f406a005e767e7")
+        property("sonar.sources", "src/main/java") 
+        property("sonar.java.binaries", "build/classes/java/main")
+        property("sonar.java.libraries", "build/libs/*.jar")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
